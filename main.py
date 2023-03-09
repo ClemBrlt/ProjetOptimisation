5c65499db181503ef3aab6d3f72316cfe44d1be9
ClementBourletLaptop <clement.bourlet1@gmail.com>
2023-03-08 19:58:36 -0500

    Execution Time

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import math
 
 import numpy as np
 import pandas as pd
+import time
 from AlgoDynamique import ProgrammationDynamique
 
+start = time.time()
 
 datas = pd.read_excel('DataProjet2023.xlsb', engine='pyxlsb', usecols='B,C,F,H,J,L,N,P', header=2, nrows=100)
 
@@ -21,14 +23,17 @@ for i in range(100):
     diffpower[i] = pmwtot - pmwreel
     totpower[i] = pmwtot
 
+end = time.time()
+
 SSE = (sum(diffpower**2))
 SSR = (sum(totpower**2))
 print(diffpower)
-print("Erreur négative maximale "+min(diffpower))
-print("Erreur positive maximale "+max(diffpower))
-print("Somme des erreurs "+sum(diffpower))
-print("Somme des erreurs carrées"+SSE)
-print("R²="(1-SSE/SSR))
+print("Erreur négative maximale: "+str(min(diffpower)))
+print("Erreur positive maximale: "+str(max(diffpower)))
+print("Somme des erreurs: "+str(sum(diffpower)))
+print("Somme des erreurs carrées: "+str(SSE))
+print("R²: "+str((1-SSE/SSR)))
+print("Temps d'éxécution: "+str(end-start))
 
 # hchute = datas['Niv Amont (m)'].loc[0] - datas['Elav (m)'].loc[0]
 # Qtot = datas['Qtot (m3/s)'].loc[0]
