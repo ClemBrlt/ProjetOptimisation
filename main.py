57afda528f17c760b63a50fa9bb78b83fc08bf43
ClementBourletLaptop <clement.bourlet1@gmail.com>
2023-03-08 19:54:51 -0500

    Better analytics

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import pandas as pd
 from AlgoDynamique import ProgrammationDynamique
@@ -6,6 +8,7 @@ from AlgoDynamique import ProgrammationDynamique
 datas = pd.read_excel('DataProjet2023.xlsb', engine='pyxlsb', usecols='B,C,F,H,J,L,N,P', header=2, nrows=100)
 
 diffpower = np.zeros(100)
+totpower = np.zeros(100)
 
 for i in range(100):
     hchute = datas['Niv Amont (m)'].loc[i] - datas['Elav (m)'].loc[i]
@@ -16,11 +19,16 @@ for i in range(100):
     pmwtot = pmw.sum()
     pmwreel = datas['P1 (MW)'].loc[i] + datas['P2 (MW)'].loc[i] + datas['P3 (MW)'].loc[i] + datas['P4 (MW)'].loc[i] + datas['P5 (MW)'].loc[i]
     diffpower[i] = pmwtot - pmwreel
+    totpower[i] = pmwtot
 
+SSE = (sum(diffpower**2))
+SSR = (sum(totpower**2))
 print(diffpower)
-print(min(diffpower))
-print(max(diffpower))
-print(sum(diffpower))
+print("Erreur négative maximale "+min(diffpower))
+print("Erreur positive maximale "+max(diffpower))
+print("Somme des erreurs "+sum(diffpower))
+print("Somme des erreurs carrées"+SSE)
+print("R²="(1-SSE/SSR))
 
 # hchute = datas['Niv Amont (m)'].loc[0] - datas['Elav (m)'].loc[0]
 # Qtot = datas['Qtot (m3/s)'].loc[0]
